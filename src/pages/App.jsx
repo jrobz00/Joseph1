ec029c0b72d5469397d491a10d33c87bea6fdd7e
josephrobinsonsimon@gmail.com <josephrobinsonsimon@gmail.com>
2025-02-02 07:17:00 +0000

    Improve Auth design and update registration/login flow

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,10 +1,10 @@
 // src/App.jsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";  // Make sure these paths are correct
-import Hero from "./components/Hero";
-import Dashboard from "./pages/Dashboard";   // Dashboard page from src/pages
-import Auth from "./pages/Auth";             // Auth page (login/register) from src/pages
+import Navbar from "../components/Navbar";  // Make sure these paths are correct
+import Hero from "../components/Hero";
+import Dashboard from "./Dashboard";   // Dashboard page from src/pages
+import Auth from "./Auth";             // Auth page (login/register) from src/pages
 
 function App() {
   return (
